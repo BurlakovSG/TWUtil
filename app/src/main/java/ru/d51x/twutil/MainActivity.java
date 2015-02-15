5a294cefd1dad20c81fc41337d81ec85b26a39d8
Dmitriy <dimonich110@gmail.com>
2015-02-15 22:27:01 +0300

    add custom handler monitor

--- app/src/main/java/ru/d51x/twutil/MainActivity.java
+++ app/src/main/java/ru/d51x/twutil/MainActivity.java
@@ -5,7 +5,8 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.util.Log;
-
+import java.util.Date;
+import java.text.SimpleDateFormat;
 
 import android.view.Menu;
 import android.view.MenuItem;
@@ -28,29 +29,23 @@ public class MainActivity extends ActionBarActivity {
     private Button id_button_sendcommand;
     private TextView id_textView_Result;
 
-    private Button button_send_command;
-    private EditText edit_command;
-    private EditText edit_arg1;
+	private EditText id_edittext_handler;
+	private Button id_button_handler;
+	private TextView id_textview_log;
+
     private TWUtil mTW, mTW2;
+	public boolean isHandlerStarted;
 
     public MainActivity() {
         this.mTW = null;
-        this.mTW2 = null;
+	    this.mTW2 = null;
+	    this.isHandlerStarted = false;
     }
 
-    static { //  !!!! what is this????
-        mHandler = new Handler() {
-            public void handleMessage(Message paramMessage) {
-                try {
-                    Log.e("AutoSettings1", String.valueOf(paramMessage.obj));
-                    Log.e("AutoSettings2", String.valueOf(paramMessage.what));
-                    Log.e("AutoSettings3", String.valueOf(paramMessage.arg1));
-                } catch (Exception localException) {
-                    Log.e("AutoSettingsERR", Log.getStackTraceString(localException));
-                }
-            }
-        };
-    }
+	static {
+
+
+	}
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,70 +59,73 @@ public class MainActivity extends ActionBarActivity {
         this.id_button_sendcommand = (Button) findViewById(R.id.id_button_sendcommand);
         this.id_textView_Result = (TextView) findViewById(R.id.id_textView_Result);
 
-        this.button_send_command = (Button) findViewById(R.id.button_send_command);
-        this.edit_command = (EditText) findViewById((R.id.editcommand));
-        this.edit_arg1 = (EditText) findViewById((R.id.edit_arg1));
-
+		this.id_edittext_handler = (EditText) findViewById(R.id.id_edittext_handler);
+	    this.id_button_handler = (Button) findViewById (R.id.id_button_handler);
+		this.id_textview_log = (TextView) findViewById (R.id.id_textview_log);
 
         this.id_button_sendcommand.setOnClickListener(new Button.OnClickListener() {
             public void onClick(View v) // клик на кнопку
             {
-                mTW = TWSetting.open();
-                mTW.addHandler("AutoSettings", mHandler);
-                if (mTW != null) {
-                    Log.e("Log", "OK: TWUtils: initialized successfully");
+	            int what = Integer.parseInt (id_edit_what.getText ().toString ());
+	            int arg1 = Integer.parseInt (id_edit_arg1.getText ().toString ());
+	            int arg2 = Integer.parseInt (id_edit_arg2.getText ().toString ());
+	            //short[] Shorts = new short[] {(short) what};
 
-                    Log.e("Log", "Edittext (what): " + String.valueOf(id_edit_what.getText().toString()));
-                    Log.e("Log", "Edittext (arg1): " + String.valueOf(id_edit_arg1.getText().toString()));
-                    Log.e("Log", "Edittext (arg2): " + String.valueOf(id_edit_arg2.getText().toString()));
+	            mTW = new TWUtil ();
+	            if (mTW.open (new short[]{(short) what}) == 0) {
+		            mTW.start ();
 
-                    try {
-                        int what = Integer.parseInt(id_edit_what.getText().toString());
-                        int arg1 = Integer.parseInt(id_edit_arg1.getText().toString());
-                        int arg2 = Integer.parseInt(id_edit_arg2.getText().toString());
-                        Log.e("Log", "Button click: " + String.valueOf(what));
-                        Log.e("Log", "Button click: " + String.valueOf(arg1));
-                        Log.e("Log", "Button click: " + String.valueOf(arg2));
-                        int res = mTW.write(what, arg1, arg2);
-                        id_textView_Result.setText(String.valueOf(res));
-                        // close session
-                        mTW.write(266, 255, 0); /// !!! зачем это???
-                    } catch (NumberFormatException e) {
-                        // введено не число
+		            int res = mTW.write (what, arg1, arg2);
+		            id_textView_Result.setText (String.valueOf (res));
+		            // close session
+		            mTW.write (what, 255, 0); /// !!! зачем это???
+	            }
+	            mTW.close ();
+	            mTW = null;
 
-                    }
-                } else {
-                    Log.e("Log", "ERROR: TWUtils: not initialized");
-                }
-
-                mTW.removeHandler("AutoSettings");
-                mTW.close();
-                mTW = null;
             }
         });
 
-        this.button_send_command.setOnClickListener(new Button.OnClickListener() {
-            public void onClick(View v) // клик на кнопку
-            {
-                String cmd = edit_command.getText().toString();
-                int arg1 = Integer.parseInt(edit_arg1.getText().toString());
-                mTW2 = new TWUtil(16);
-                if (mTW2 != null) {
-                    try {
-                        if (mTW2.open( new short[]{(short) 260, (short) 262, (short) 263, (short) 266, (short) 274, (short) 513, (short) 514, (short) 515, (short) 769, (short) 770, (short) 1281, (short) -25088, (short) -25071, (short) -24816, (short) -24805, (short) -24804} ) == 0) {
-                            mTW2.start();
-                            mTW2.write(40730, arg1, 0, cmd);
-                            mTW2.stop();
-                            mTW2.close();
-                        }
+	    this.id_button_handler.setOnClickListener(new Button.OnClickListener () {
+		    public void onClick (View v) // клик на кнопку
+		    {
+			    final int xwhat = Integer.parseInt (id_edittext_handler.getText ().toString ());
+			    if (isHandlerStarted) {
+				    // stop handler
+				    // stop and close TWUtil
+				    mTW2.removeHandler ("TWUtilHandler");
+				    mTW2.stop ();
+				    mTW2.close ();
+				    mTW2 = null;
+				    isHandlerStarted = false;
+				    id_button_handler.setText ("Start Handler");
+			    } else {
+				    // open and start TWUtil
+				    mTW2 = new TWUtil ();
+				    if (mTW2.open (new short[]{(short) xwhat}) == 0) {
+					    mTW2.start ();
+					    mTW2.addHandler ("TWUtilHandler", new Handler () {
+						    public void handleMessage(Message msg) {
+							    try {
+								    if (msg.what == xwhat ) {
+										    Date date = new Date();
+										    SimpleDateFormat ft = new SimpleDateFormat ("yyyy.MMdd. hh:mm:ss");
+											String str = String.format("%s:  Message: what: %i   arg1: %i  arg2: %i\n", ft.format(date), msg.what, msg.arg1, msg.arg2);
+										    id_textview_log.append ( str );
+								    }
+							    } catch (Exception e) {
+							    }
+						    }
+						});
+					    mTW2.write (xwhat, 255);
+						isHandlerStarted = true;
+					    id_button_handler.setText ("Stop Handler");
+				    }
+				    // start handler
 
-                    } catch (NumberFormatException e) {
-                        // введено не число
-
-                    }
-                }
-            }
-        });
+			    }
+		    }
+	    });
     }
 
 
